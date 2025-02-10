98223fb9a690cac0278596ad6e7f4fcce1d6397c
prasetyadwiagustie <52117421+prasetyadwiagustie@users.noreply.github.com>
2025-02-10 08:54:52 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,27 +1,3 @@
-<?php
-	@ob_start();
-	session_start();
-	if(isset($_POST['proses'])){
-		require 'config.php';
-			
-		$user = strip_tags($_POST['user']);
-		$pass = strip_tags($_POST['pass']);
-
-		$sql = 'select member.*, login.user, login.pass
-				from member inner join login on member.id_member = login.id_member
-				where user =? and pass = md5(?)';
-		$row = $config->prepare($sql);
-		$row -> execute(array($user,$pass));
-		$jum = $row -> rowCount();
-		if($jum > 0){
-			$hasil = $row -> fetch();
-			$_SESSION['admin'] = $hasil;
-			echo '<script>alert("Login Sukses");window.location="index.php"</script>';
-		}else{
-			echo '<script>alert("Login Gagal");history.go(-1);</script>';
-		}
-	}
-?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -29,50 +5,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="Dashboard">
-    <meta name="keyword">
+    <meta name="keyword" content="">
 
     <title>Login To Admin</title>
 
     <!-- Bootstrap core CSS -->
     <link href="assets/css/bootstrap.css" rel="stylesheet">
-    <!--external css-->
+    <!-- External CSS -->
     <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
-        
-    <!-- Custom styles for this template -->
     <link href="assets/css/style.css" rel="stylesheet">
     <link href="assets/css/style-responsive.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
   </head>
 
   <body style="background:#004643;color:#fff;">
+    <div id="login-page" style="padding-top:3pc;">
+      <div class="container">
+        <form class="form-login">
+          <h2 class="form-login-heading">Aplikasi POS</h2>
+          <div class="login-wrap">
+            <input type="text" class="form-control" name="user" placeholder="User ID" autofocus>
+            <br>
+            <input type="password" class="form-control" name="pass" placeholder="Password">
+            <br>
+            <button class="btn btn-primary btn-block" type="button" onclick="login()">
+              <i class="fa fa-lock"></i> SIGN IN
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
 
-      <!-- **********************************************************************************************************************************************************
-      MAIN CONTENT
-      *********************************************************************************************************************************************************** -->
-
-	  <div id="login-page" style="padding-top:3pc;">
-	  	<div class="container">
-		      <form class="form-login" method="POST">
-		        <h2 class="form-login-heading">Aplikasi POS</h2>
-		        <div class="login-wrap">
-		            <input type="text" class="form-control" name="user" placeholder="User ID" autofocus>
-		            <br>
-		            <input type="password" class="form-control" name="pass" placeholder="Password">
-		            <br>
-		            <button class="btn btn-primary btn-block" name="proses" type="submit"><i class="fa fa-lock"></i> SIGN IN</button>
-		        </div>
-		      </form>	  	
-	  	
-	  	</div>
-	  </div>
-    <!-- js placed at the end of the document so the pages load faster -->
+    <!-- JS for Login Simulation -->
+    <script>
+      function login() {
+        alert("Login functionality is disabled on static pages.");
+      }
+    </script>
+    
+    <!-- JS Libraries -->
     <script src="assets/js/jquery.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
   </body>
 </html>
-
